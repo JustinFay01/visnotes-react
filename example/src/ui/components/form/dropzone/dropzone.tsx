f2cd5b211ce4bbf7353ee8d17f8f226ed6ff0986
Justin Fay <justin.fay@hope.edu>
2024-12-12 13:09:32 -0500

    Refactor OcrDropzone component by replacing CSS with styled components for improved styling and maintainability

--- example/src/ui/components/form/dropzone/dropzone.tsx
+++ example/src/ui/components/form/dropzone/dropzone.tsx
@@ -1,23 +1,52 @@
 import { CloudUpload } from "@mui/icons-material";
 import React, { useCallback } from "react";
 import { Accept, FileRejection, useDropzone } from "react-dropzone";
-import { Box } from "@mui/material";
+import { Box, styled } from "@mui/material";
 import "./dropzone.css";
+import { WithSx } from "@/ui/props";
 
-interface OcrDropzoneProps {
+type OcrDropzoneProps = WithSx & {
   children: React.ReactNode;
   className?: string;
   style?: React.CSSProperties;
   accept?: Accept;
   handleAcceptedFiles?: (files: File[]) => void;
   handleRejectedFiles?: (files: FileRejection[]) => void;
-}
+};
+
+const StyledDropzone = styled(Box)<{ isDragActive: boolean }>(
+  ({ theme, isDragActive }) => ({
+    border: `2px solid ${
+      isDragActive ? theme.palette.primary.main : "transparent"
+    }`,
+    backgroundColor: isDragActive
+      ? theme.palette.action.hover
+      : theme.palette.background.default,
+    transition: "border 0.2s ease-out, background-color 0.2s ease-out",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: theme.spacing(2),
+    borderRadius: theme.shape.borderRadius,
+  })
+);
+
+const StyledMessage = styled(Box)<{ isDragActive: boolean }>(
+  ({ theme, isDragActive }) => ({
+    color: theme.palette.text.secondary,
+    display: isDragActive ? "block" : "none",
+    textAlign: "center",
+    marginTop: theme.spacing(2),
+  })
+);
 
 const OcrDropzone: React.FC<OcrDropzoneProps> = ({
   className,
   children,
   style,
   accept,
+  sx,
   handleAcceptedFiles,
   handleRejectedFiles,
 }) => {
@@ -42,23 +71,22 @@ const OcrDropzone: React.FC<OcrDropzoneProps> = ({
   });
 
   return (
-    <Box
-      aria-label="file dropzone"
-      className={`dropzone ${className ?? ""} ${
-        isDragActive ? "active" : "inactive"
-      }`}
+    <StyledDropzone
+      className={`dropzone ${className ?? ""}`}
+      isDragActive={isDragActive}
       {...getRootProps({
-        style: { ...style },
+        style,
       })}
+      sx={sx} // Users can pass the sx prop to extend or override styles
     >
       <input {...getInputProps()} />
       {children}
 
-      <Box className={`message ${isDragActive ? "visible" : ""}`}>
+      <StyledMessage className="message" isDragActive={isDragActive}>
         <CloudUpload />
         Drop files to upload
-      </Box>
-    </Box>
+      </StyledMessage>
+    </StyledDropzone>
   );
 };
 
