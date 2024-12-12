982102de9f67bc3064a7921e653193bb486287e1
Justin Fay <justinfay501@gmail.com>
2024-12-12 15:36:02 -0500

    Enhance StyledDropzone component by adding relative positioning and overflow hidden for improved layout and focus visibility

--- example/src/ui/components/form/dropzone/dropzone.tsx
+++ example/src/ui/components/form/dropzone/dropzone.tsx
@@ -16,6 +16,8 @@ type OcrDropzoneProps = WithSx & {
 
 const StyledDropzone = styled(Box)<{ isDragActive: boolean }>(
   ({ theme, isDragActive }) => ({
+    position: "relative",
+    overflow: "hidden",
     border: `2px solid ${
       isDragActive ? theme.palette.primary.main : "transparent"
     }`,
@@ -29,6 +31,10 @@ const StyledDropzone = styled(Box)<{ isDragActive: boolean }>(
     justifyContent: "center",
     padding: theme.spacing(2),
     borderRadius: theme.shape.borderRadius,
+    "&:focus-visible": {
+      outline: `2px solid ${theme.palette.primary.main}`,
+      borderColor: theme.palette.primary.main,
+    },
   })
 );
 
