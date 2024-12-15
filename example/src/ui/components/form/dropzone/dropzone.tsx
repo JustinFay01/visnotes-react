9d2b4607f7b7dbe65715ff099f7dd1fa8e876079
Justin Fay <justinfay501@gmail.com>
2024-12-15 10:45:50 -0500

    Passed custom prop to a styled component!

--- example/src/ui/components/form/dropzone/dropzone.tsx
+++ example/src/ui/components/form/dropzone/dropzone.tsx
@@ -11,54 +11,54 @@ type OcrDropzoneProps = WithSx & {
   handleRejectedFiles?: (files: FileRejection[]) => void;
 };
 
-const StyledDropzone = styled(Box)<{ isDragActive: boolean }>(
-  ({ theme, isDragActive }) => ({
-    position: "relative",
-    overflow: "hidden",
-    border: `2px solid ${
-      isDragActive ? theme.palette.primary.main : "transparent"
-    }`,
-    backgroundColor: isDragActive
-      ? theme.palette.action.hover
-      : theme.palette.background.default,
-    transition: "border 0.2s ease-out, background-color 0.2s ease-out",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: theme.spacing(2),
-    borderRadius: theme.shape.borderRadius,
-    "&:focus-visible": {
-      outline: `2px solid ${theme.palette.primary.main}`,
-      borderColor: theme.palette.primary.main,
-    },
-  })
-);
+const StyledDropzone = styled(Box, {
+  shouldForwardProp: (prop) => prop !== "isDragActive",
+})<{ isDragActive: boolean }>(({ theme, isDragActive }) => ({
+  position: "relative",
+  overflow: "hidden",
+  border: `2px solid ${
+    isDragActive ? theme.palette.primary.main : "transparent"
+  }`,
+  backgroundColor: isDragActive
+    ? theme.palette.action.hover
+    : theme.palette.background.default,
+  transition: "border 0.2s ease-out, background-color 0.2s ease-out",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  padding: theme.spacing(2),
+  borderRadius: theme.shape.borderRadius,
+  "&:focus-visible": {
+    outline: `2px solid ${theme.palette.primary.main}`,
+    borderColor: theme.palette.primary.main,
+  },
+}));
 
-const StyledMessage = styled(Box)<{ isDragActive: boolean }>(
-  ({ theme, isDragActive }) => ({
-    color: theme.palette.text.secondary,
-    backgroundColor: theme.palette.primary.main,
-    fontWeight: 700,
-    height: 48,
-    borderRadius: "24px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    gap: theme.spacing(1),
-    width: "340px",
-    position: "absolute",
-    left: "50%",
-    zIndex: 10,
-    transform: `${
-      isDragActive
-        ? "translateX(-50%) scale(1)"
-        : "translateX(-50%) translateY(100px) scale(0.8)"
-    }`,
-    opacity: isDragActive ? 1 : 0,
-    transition: "transform 0.2s ease-out, opacity 0.2s ease-out",
-  })
-);
+const StyledMessage = styled(Box, {
+  shouldForwardProp: (prop) => prop !== "isDragActive",
+})<{ isDragActive: boolean }>(({ theme, isDragActive }) => ({
+  color: theme.palette.text.secondary,
+  backgroundColor: theme.palette.primary.main,
+  fontWeight: 700,
+  height: 48,
+  borderRadius: "24px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: theme.spacing(1),
+  width: "340px",
+  position: "absolute",
+  left: "50%",
+  zIndex: 10,
+  transform: `${
+    isDragActive
+      ? "translateX(-50%) scale(1)"
+      : "translateX(-50%) translateY(100px) scale(0.8)"
+  }`,
+  opacity: isDragActive ? 1 : 0,
+  transition: "transform 0.2s ease-out, opacity 0.2s ease-out",
+}));
 
 const OcrDropzone = ({
   children,
