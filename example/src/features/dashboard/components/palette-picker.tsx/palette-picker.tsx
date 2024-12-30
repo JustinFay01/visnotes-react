2d5d89934134e2e90e4d40d21dae8b5f27e10f1b
Justin Fay <justinfay501@gmail.com>
2024-12-29 23:38:09 -0500

    Comments

--- example/src/features/dashboard/components/palette-picker.tsx/palette-picker.tsx
+++ example/src/features/dashboard/components/palette-picker.tsx/palette-picker.tsx
@@ -24,11 +24,11 @@ export const PalettePicker = ({ colors, setColors }: PalettePickerProps) => {
         {colors.map((color) => (
           <motion.div
             key={color}
-            layout
-            animate={{ opacity: 1, scale: 1 }}
-            initial={{ opacity: 0, scale: 0 }}
+            layout // Enable layout animations so elements will slide to their new position
+            animate={{ opacity: 1, scale: 1 }} // Animate to opacity: 1 (visible) and scale: 1 (100% size)
+            initial={{ opacity: 0, scale: 0 }} // so it grows from 0 to 1
             transition={{ duration: 0.2 }}
-            exit={{ opacity: 0, scale: 0 }}
+            exit={{ opacity: 0, scale: 0 }} // so it shrinks to 0 and then disappears
           >
             <Grid size={2} sx={{ minWidth: "120px", maxWidth: "120px" }}>
               <ColorChip color={color} onDelete={handleDelete} />
