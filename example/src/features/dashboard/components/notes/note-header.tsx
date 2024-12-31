e0bb8369ed38a52f32aa40d63a4d0a0043ce68b6
Justin Fay <justin.fay@hope.edu>
2024-12-30 19:38:52 -0500

    Refactor NoteHeader component layout; replace FlexRow with Stack for improved responsiveness

--- example/src/features/dashboard/components/notes/note-header.tsx
+++ example/src/features/dashboard/components/notes/note-header.tsx
@@ -1,8 +1,8 @@
-import { FlexColumn, FlexRow, FlexSpacer } from "@/ui/layout/flexbox";
-import DeleteIcon from "@mui/icons-material/Delete";
-import { Button, Typography } from "@mui/material";
+import { FlexColumn, FlexSpacer } from "@/ui/layout/flexbox";
 import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
 import TroubleshootIcon from "@mui/icons-material/Troubleshoot";
+import { Button, Stack, Typography } from "@mui/material";
 
 type NoteHeaderProps = {
   onCreate: () => void;
@@ -16,34 +16,40 @@ export const NoteHeader = ({
   onDelete,
   onAnalyze,
   notesSelected,
-}: NoteHeaderProps) => (
-  <FlexColumn>
-    <Typography variant="h5" sx={{ marginTop: 2 }}>
-      Saved Notes
-    </Typography>
-    <Typography variant="body1">View your saved notes here.</Typography>
+}: NoteHeaderProps) => {
+  return (
+    <FlexColumn>
+      <Typography variant="h5" sx={{ marginTop: 2 }}>
+        Saved Notes
+      </Typography>
+      <Typography variant="body1">View your saved notes here.</Typography>
 
-    <FlexRow spacing={2} sx={{ marginTop: 2 }}>
-      <FlexSpacer />
-      <Button variant="contained" startIcon={<AddIcon />} onClick={onCreate}>
-        Create
-      </Button>
-      <Button
-        variant="contained"
-        startIcon={<TroubleshootIcon />}
-        onClick={onAnalyze}
-        disabled={!notesSelected}
+      <Stack
+        spacing={2}
+        sx={{ marginTop: 2 }}
+        direction={{ sm: "column", md: "row" }}
       >
-        Analyze
-      </Button>
-      <Button
-        variant="outlined"
-        startIcon={<DeleteIcon />}
-        onClick={onDelete}
-        disabled={!notesSelected}
-      >
-        Delete
-      </Button>
-    </FlexRow>
-  </FlexColumn>
-);
+        <FlexSpacer />
+        <Button variant="contained" startIcon={<AddIcon />} onClick={onCreate}>
+          Create
+        </Button>
+        <Button
+          variant="contained"
+          startIcon={<TroubleshootIcon />}
+          onClick={onAnalyze}
+          disabled={!notesSelected}
+        >
+          Analyze
+        </Button>
+        <Button
+          variant="outlined"
+          startIcon={<DeleteIcon />}
+          onClick={onDelete}
+          disabled={!notesSelected}
+        >
+          Delete
+        </Button>
+      </Stack>
+    </FlexColumn>
+  );
+};
