69c4d68dc55b52a7bb6505fd7fef2b2ae848910f
Justin Fay <justin.fay@hope.edu>
2024-12-30 22:07:59 -0500

    Uncomment CSS import for react-color-palette in ColorPickerButton component

--- example/src/features/dashboard/components/palette-picker.tsx/color-picker-button.tsx
+++ example/src/features/dashboard/components/palette-picker.tsx/color-picker-button.tsx
@@ -3,7 +3,8 @@ import AddIcon from "@mui/icons-material/Add";
 import { Button, Popover } from "@mui/material";
 import { useState } from "react";
 import { ColorPicker, IColor, useColor } from "react-color-palette";
-// import "react-color-palette/css";
+// @ts-ignore
+import "react-color-palette/css";
 import { getTextColor } from "../../util/color-util";
 
 type ColorPickerButtonProps = {
