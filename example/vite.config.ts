215f186c7c5d525df3decd078b627f30f10f39f5
Justin Fay <justin@srv684136.hstgr.cloud>
2025-01-03 14:01:33 +0000

    Comment out Traefik labels in docker-stack.yml and change Vite server port to 8080

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
-    port: 3000,
+    port: 8080,
     host: true,
   },
 });
