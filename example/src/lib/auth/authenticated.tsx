aa68102f5f41243b9f6d52e7828e1529c63d8f31
Justin Fay <justinfay501@gmail.com>
2025-01-23 23:29:23 -0500

    Refactor authentication logic to use Auth0 directly and remove custom useAuth hook

--- example/src/lib/auth/authenticated.tsx
+++ example/src/lib/auth/authenticated.tsx
@@ -1,13 +1,12 @@
 import { WithChildren } from "@/ui/props";
-import { withAuthenticationRequired } from "@auth0/auth0-react";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 import { useEffect } from "react";
 import { addAuthToken } from "../axios";
-import { useAuth } from "./use-auth";
 
 type AuthenticatedProps = WithChildren;
 
 export const Authenticated = ({ children }: AuthenticatedProps) => {
-  const { getAccessTokenSilently } = useAuth();
+  const { getAccessTokenSilently } = useAuth0();
 
   useEffect(() => {
     getAccessTokenSilently().then(addAuthToken);
